729815d8bf4a24bb8a2c695f194e45713bf2fc85
beardyman <nornholdj@gmail.com>
2016-04-05 16:45:15 -0400

    moved env.es6: true into base config

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = {
+  "env": {
+    "es6": true
+  },
   "extends": "eslint:recommended",
   "rules": {
     "strict": "warn",
--- ui.js
+++ ui.js
@@ -1,7 +1,6 @@
 module.exports = {
   "env": {
     "browser": true,
-    "es6": true,
     "phantomjs": true,
     "protractor": true
   },
