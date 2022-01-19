a7c99ce4c49c5194663667dc00a5a18fff708d70
Greg Walls <greg.walls@sparkpost.com>
2022-01-19 12:49:21 -0500

    SAA-466 Updated all deps

--- index.js
+++ index.js
@@ -88,11 +88,12 @@ module.exports = {
     },
     {
       files: ['*.ts'],
-      extends:
-      [
+      extends: [
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
+        'prettier',
+        // 'prettier/@typescript-eslint',
+        // Error: "prettier/@typescript-eslint" has been merged into "prettier" in eslint-config-prettier 8.0.0. See: https://github.com/prettier/eslint-config-prettier/blob/main/CHANGELOG.md#version-800-2021-02-21
         'plugin:prettier/recommended'
       ],
       env: {
