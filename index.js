da32cd232dce3ac08734b268ecb80232c46e1c8f
beardyman <jordan.nornhold@sparkpost.com>
2022-01-26 09:15:07 -0500

    SAA-466 removed unneeded comments

--- index.js
+++ index.js
@@ -92,8 +92,6 @@ module.exports = {
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'prettier',
-        // 'prettier/@typescript-eslint',
-        // Error: "prettier/@typescript-eslint" has been merged into "prettier" in eslint-config-prettier 8.0.0. See: https://github.com/prettier/eslint-config-prettier/blob/main/CHANGELOG.md#version-800-2021-02-21
         'plugin:prettier/recommended'
       ],
       env: {
