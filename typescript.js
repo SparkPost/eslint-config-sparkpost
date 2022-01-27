aa6f98c3845f877f84ed1bc86fc47208ec3b225b
beardyman <jordan.nornhold@sparkpost.com>
2022-01-27 11:03:49 -0500

    SAA-466 removed deprecated TS plugin for prettier

--- typescript.js
+++ typescript.js
@@ -12,7 +12,6 @@ module.exports = {
     'sparkpost/index',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'plugin:prettier/recommended'
   ],
   rules: {
